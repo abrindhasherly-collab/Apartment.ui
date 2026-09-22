4d9320af7646b57ed8d34775818486b544afbb4c
asvinthsathiyadhas-cmyk <asvinthsathiyadhas@gmail.com>
2026-09-22 12:04:50 +0530

    navbar updated

--- src/app/shared/navbar/navbar.spec.ts
+++ src/app/shared/navbar/navbar.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NavbarComponent } from './navbar';
+import { Navbar } from './navbar';
 
 describe('Navbar', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+  let component: Navbar;
+  let fixture: ComponentFixture<Navbar>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent],
+      imports: [Navbar],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(Navbar);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,16 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  Component,
+  signal
+} from '@angular/core';
+
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+
   imports: [
     RouterLink,
     RouterLinkActive
   ],
+
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class NavbarComponent {
+export class Navbar {
 
+  menuOpen = signal(false);
+
+  constructor(
+    private router: Router
+  ) {}
+
+  toggleMenu(): void {
+    this.menuOpen.update(
+      value => !value
+    );
+  }
+
+  closeMenu(): void {
+    this.menuOpen.set(false);
+  }
+
+  logout(): void {
+
+    localStorage.removeItem('token');
+
+    this.closeMenu();
+
+    this.router.navigate(['/login']);
+  }
 }
